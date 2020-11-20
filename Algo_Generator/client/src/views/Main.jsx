fa335c2f04c84d82bf4f6c2f0c811158fbabd893
Schneider Bertrand <snite1993@gmail.com>
2020-11-20 14:21:26 -0600

    Switch case added. Setting up for selection

--- Algo_Generator/client/src/views/Main.jsx
+++ Algo_Generator/client/src/views/Main.jsx
@@ -6,22 +6,51 @@ const Main = props => {
 
     const generate = () => {
         const generator = document.querySelector(".generatorArea");
-            let timesrun = 0;
-           let interval= setInterval(() => {
-             timesrun += 1;
-             let position = Math.floor(Math.random() * 401);
-             generator.scrollTo(0, position);
-             if(timesrun === 50 ){
-                clearInterval(interval);
-             }
+        let timesrun = 0;
+        let interval = setInterval(() => {
+            timesrun += 1;
+            let position = Math.floor(Math.random() * 401);
 
-            }, 80)
+            switch (position) {
+                case position < 26:
+                    position = 0;
+                    break;
+                case position > 26:
+                    position = 57;
+                    break;
+                case position > 76:
+                    position = 114;
+                    break;
+                case position > 76:
+                    position = 171;
+                    break;
+                case position > 126:
+                    position = 228;
+                    break;
+                case position > 176:
+                    position = 285;
+                    break;
+                case position > 226:
+                    position = 342;
+                    break;
+                case position > 276:
+                    position = 400;
+                    break;
+                default:
+                    break;
+            }
+            generator.scrollTo(0, position);
+            if (timesrun === 50) {
+                clearInterval(interval);
+            }
+
+        }, 80)
     }
 
     return (
         <div id="generatorContainer">
             <AlgorithmGenerator></AlgorithmGenerator>
-            <button onClick = {generate} className="btn btn-danger mt-2 col-sm-3 mx-auto lead">Click Me!</button>
+            <button onClick={generate} className="btn btn-danger mt-2 col-sm-3 mx-auto lead">Click Me!</button>
         </div>
     )
 };
