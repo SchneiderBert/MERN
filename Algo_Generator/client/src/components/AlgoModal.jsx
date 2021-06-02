82a4f8ef1f961808a526aeeb0eec20c179345ed4
Schneider Bertrand <snite1993@gmail.com>
2021-06-02 12:18:40 -0500

    Responsive Design && Remove add link

--- Algo_Generator/client/src/components/AlgoModal.jsx
+++ Algo_Generator/client/src/components/AlgoModal.jsx
@@ -4,10 +4,44 @@ import ReactDom from 'react-dom'
 
 const AlgoModal = props => {
 
+    if (!props.open) return null;
 
     return ReactDom.createPortal(
-        <div id="modal">
+        <div id="modal-container">
 
-        </div>, document.getElementById('portal')
+            <div id="modal">
+                <div id="modal-content">
+                    <div className="algo-info">
+                        <h3 className="algo-name">
+                            kjlkhkj
+                    </h3>
+                        <p className="algo-description">
+                            Given array and an index into array, remove and
+                            return the array value at that index. Do this
+                            without using built-in array methods except
+                            pop(). Think of popFront(arr) as equivalent
+                            to removeAt(arr,0).
+                    </p>
+                    </div>
+                    <div id="algo-videos">
+                        <div className="problem-container vid-container">
+                            <h4 className="problem-header">Problem:</h4>
+                            <img src="" alt="" className="problem-video video" />
+
+                        </div>
+
+                        <div className="answer-container vid-container">
+                            <h4 className="answer-header">Answer:</h4>
+                            <img src="" alt="" className="answer-video video" />
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        , document.getElementById('portal')
     )
 }
+
+export default AlgoModal;
--- Algo_Generator/client/src/views/Main.jsx
+++ Algo_Generator/client/src/views/Main.jsx
@@ -3,11 +3,12 @@ import AlgorithmGenerator from '../components/AlgorithmGenerator';
 import {Link} from '@reach/router'
 import axios from 'axios';
 import { Typography } from '@material-ui/core';
+import AlgoModal from '../components/AlgoModal';
 
 
 const Main = props => {
     const [algoList, setAlgoList] = useState([])
-    const [selectedAlgo, setSelectedAlgo] = useState()
+    const [generarorselectedAlgo, setGeneratorSelectedAlgo] = useState()
     const [isOpen, setIsOpen] = useState(false)
     useEffect(()=> {
         axios.get('http://localhost:8000/api/algorithms')
@@ -25,24 +26,33 @@ const Main = props => {
         const generator = document.querySelector(".generatorArea");
         let timesrun = 0;
         let selectedAlgo;
+        let logicAlgo;
         let interval = setInterval(() => {
             timesrun += 1;
             let position = Math.floor(Math.random() * 401);
             if (position > 326) {
+                logicAlgo = algoList[7-1];
                 selectedAlgo = document.getElementById("algo-7");
             } else if (position > 276) {
+                logicAlgo = algoList[7-1];
                 selectedAlgo = document.getElementById("algo-7");
             } else if (position > 226) {
+                logicAlgo = algoList[6-1];
                 selectedAlgo = document.getElementById("algo-6");
             } else if (position > 176) {
+                logicAlgo = algoList[5-1];
                 selectedAlgo = document.getElementById("algo-5");
             } else if (position > 126) {
+                logicAlgo = algoList[4-1];
                 selectedAlgo = document.getElementById("algo-4");
             } else if (position > 76) {
+                logicAlgo = algoList[3-1];
                 selectedAlgo = document.getElementById("algo-3");
             } else if (position > 26) {
+                logicAlgo = algoList[2-1];
                 selectedAlgo = document.getElementById("algo-2");
             } else if (position < 26) {
+                logicAlgo = algoList[1-1];
                 selectedAlgo = document.getElementById("algo-1");
             }
             // console.log(selectedAlgo);
@@ -54,18 +64,22 @@ const Main = props => {
                 setTimeout(() => {
 
                     selectedAlgo.className = "algoSelection";
-                },500);
+                },500)
 
             }
         }, 80)
+
+        setTimeout(()=> setIsOpen(true), 6000)
+  
     }
+    
 
     return (
         <div id="generatorContainer">
-            <Link to='/add'><Typography variant='h4'>Add</Typography> </Link>
+            {/* <Link to='/add'><Typography variant='h4'>Add</Typography> </Link> */}
             <AlgorithmGenerator algos={algoList}></AlgorithmGenerator>
             <button onClick={generate} className="btn btn-danger mt-2 col-sm-3 mx-auto lead">Click Me!</button>
-            
+            <AlgoModal open={isOpen} Algo={generarorselectedAlgo} />
         </div>
     )
 };
